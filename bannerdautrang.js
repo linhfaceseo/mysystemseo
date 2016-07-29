9481dc02747c9118a4272d9da262344e549d9d48
Linh Nguyễn Faceseo <linhnguyen@faceseo.vn>
2016-07-29 18:23:40 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -1,8 +1,8 @@
 //please contact me giaiphapthuonghieu.org@gmail.com
 //website http://giaiphapthuonghieu.vn or http://faceseo.vn
 
-(function($) {
-    $.fn.linhnguyen = function(options) {        
+(function(JQuery) {
+    JQuery.fn.linhnguyen = function(options) {        
         var defaults = {  
 	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
 		    animationspeed: 300, //how fast animtions are
@@ -10,40 +10,40 @@
 		    dismissmodalclass: 'close-linhnguyen-modal' //the class of a button or element that will close an open modal
     	}; 
 
-        var options = $.extend({}, defaults, options); 
+        var options = JQuery.extend({}, defaults, options); 
 	
         return this.each(function() {
-        	var modal = $(this),
+        	var modal = JQuery(this),
         		topMeasure  = parseInt(modal.css('top')),
 				topOffset = modal.height() + topMeasure,
           		locked = false,
-				modalBG = $('.linhnguyen-modal-bg');
+				modalBG = JQuery('.linhnguyen-modal-bg');
 
 			if(modalBG.length == 0) {
-				modalBG = $('<div class="linhnguyen-modal-bg" />').insertAfter(modal);
+				modalBG = JQuery('<div class="linhnguyen-modal-bg" />').insertAfter(modal);
 			}		    
 			modal.bind('linhnguyen:open', function () {
 			  modalBG.unbind('click.modalEvent');
-				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				JQuery('.' + options.dismissmodalclass).unbind('click.modalEvent');
 				if(!locked) {
 					lockModal();
 					if(options.animation == "fadeAndPop") {
-						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modal.css({'top': JQuery(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
 						modalBG.fadeIn(options.animationspeed/2);
 						modal.delay(options.animationspeed/2).animate({
-							"top": $(document).scrollTop()+topMeasure + 'px',
+							"top": JQuery(document).scrollTop()+topMeasure + 'px',
 							"opacity" : 1
 						}, options.animationspeed,unlockModal());					
 					}
 					if(options.animation == "fade") {
-						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': JQuery(document).scrollTop()+topMeasure});
 						modalBG.fadeIn(options.animationspeed/2);
 						modal.delay(options.animationspeed/2).animate({
 							"opacity" : 1
 						}, options.animationspeed,unlockModal());					
 					} 
 					if(options.animation == "none") {
-						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modal.css({'visibility' : 'visible', 'top':JQuery(document).scrollTop()+topMeasure});
 						modalBG.css({"display":"block"});	
 						unlockModal()				
 					}
@@ -57,7 +57,7 @@
 					if(options.animation == "fadeAndPop") {
 						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
 						modal.animate({
-							"top":  $(document).scrollTop()-topOffset + 'px',
+							"top":  JQuery(document).scrollTop()-topOffset + 'px',
 							"opacity" : 0
 						}, options.animationspeed/2, function() {
 							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
@@ -84,7 +84,7 @@
     	modal.trigger('linhnguyen:open')
 			
 			//Close Modal Listeners
-			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			var closeButton = JQuery('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
 			  modal.trigger('linhnguyen:close')
 			});
 			
@@ -94,7 +94,7 @@
 				  modal.trigger('linhnguyen:close')
 				});
 			}
-			$('body').keyup(function(e) {
+			JQuery('body').keyup(function(e) {
         		if(e.which===27){ modal.trigger('linhnguyen:close'); } // 27 is the keycode for the Escape key
 			});
 			
@@ -109,15 +109,15 @@
     }//orbit plugin call
 })(jQuery);
         
-$(window).load(function() {
+JQuery(window).load(function() {
 		if(document.cookie.indexOf("adf") == -1)
 		{
 			document.cookie = "adfpopunder1=adf";
 			var divpopup = document.createElement("div");
-			$(divpopup).attr('id', 'myModal');
-			$(divpopup).attr('class', 'linhnguyen-modal');
-			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://faceseo.vn/bannerweb/hoc-seo-chuyen-sau.jpg' width='500px'/><br/>13 Kỹ thuật đẩy top vượt trội</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
-			$('#myModal').linhnguyen($('#myModal').data());
+			JQuery(divpopup).attr('id', 'myModal');
+			JQuery(divpopup).attr('class', 'linhnguyen-modal');
+			JQuery( "body" ).append(divpopup);
+			JQuery("#myModal").html("<a href='http://goo.gl/rKJWvA' target='_blank'><img src='http://faceseo.vn/bannerweb/hoc-seo-chuyen-sau.jpg' width='500px'/><br/>13 Kỹ thuật đẩy top vượt trội</a>. Học tại nhà CEO Faceseo. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			JQuery('#myModal').linhnguyen(JQuery('#myModal').data());
 		}
 });
